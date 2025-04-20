9bb7ef9ae8255f42314f0b3a1f38c8e3e06cdbd0
ArnabMdev <mondalarnab15@gmail.com>
2025-04-20 16:43:25 +0530

    Initial Commit

---
+++ src/main/java/com/ArnabMdev/ArkaneGames/ArkaneGamesApplication.java
@@ -0,0 +1,13 @@
+package com.ArnabMdev.ArkaneGames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkaneGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArkaneGamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ArnabMdev/ArkaneGames/ArkaneGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ArnabMdev.ArkaneGames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArkaneGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
